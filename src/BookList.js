63128b6a9add3c1f27ac85589f4607f896f5f7ad
Aden Navi <aden7x@gmail.com>
2018-11-08 21:28:23 -0800

    add more shelves to booklist component

--- src/BookList.js
+++ src/BookList.js
@@ -6,52 +6,110 @@ class BookList extends Component {
   render() {
 
     return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
 
-          {this.props.books.map((books) => (
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks.thumbnail})` }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+          {/*currently reading shelf*/}
+            <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books.filter((book) => {
+                return book.shelf === 'currentlyReading';
+              }).map((book) => (
+                <li key={book.industryIdentifiers[0].identifier}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors[0]}</div>
                   </div>
-                </div>
-                <div className="book-title">{books.title}</div>
-                <div className="book-authors">{books.authors[0]}</div>
-              </div>
-            </li>
-          ))}
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">Ender's Game</div>
-              <div className="book-authors">Orson Scott Card</div>
+                </li>
+              ))}
+            </ol>
             </div>
-          </li>
-        </ol>
+            </div>
+
+            {/*want to read shelf*/}
+              <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+              <ol className="books-grid">
+                {this.props.books.filter((book) => {
+                  return book.shelf === 'wantToRead';
+                }).map((book) => (
+                  <li key={book.industryIdentifiers[0].identifier}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors[0]}</div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+              </div>
+              </div>
+
+              {/*read shelf*/}
+                <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.books.filter((book) => {
+                    return book.shelf === 'read';
+                  }).map((book) => (
+                    <li key={book.industryIdentifiers[0].identifier}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors[0]}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+                </div>
+                </div>
+
+          </div>
+        </div>
       </div>
-    </div>
+
   )
   }
 }
